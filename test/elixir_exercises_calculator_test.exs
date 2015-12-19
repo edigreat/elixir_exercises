98054406e09acb33143f4a4328bf5a229ffa0e6f
edigreat <heriberto.galdamez@gmail.com>
2015-12-19 17:20:10 -0600

    fixbug , replace setup function with setup_all

--- test/elixir_exercises_calculator_test.exs
+++ test/elixir_exercises_calculator_test.exs
@@ -1,11 +1,12 @@
 defmodule ElixirExercises.CalculatorTest do 
   use ExUnit.Case
   import ElixirExercises.Calculator, only: [start: 0 , value: 1 ]
-  setup do 
-    context = %{ calculator_pid: start}
+  setup_all  do 
+      context = %{ calculator_pid: start}
     {:ok, context}
   end
   test "The initial value must be 0", %{calculator_pid: calculator_pid}  do
-  value(calculator_pid) == 0
+    IO.puts inspect calculator_pid
+    value(calculator_pid) == 0
   end
 end
