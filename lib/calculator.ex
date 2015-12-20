c41b1ca049b678b6a67937b64128f8cfda4629bd
edigreat <heriberto.galdamez@gmail.com>
2015-12-19 18:00:17 -0600

    function add added

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -7,9 +7,13 @@ defmodule ElixirExercises.Calculator do
     send(calculator_pid,:value)
   end
 
+  def add(calculator_pid, value) do
+    send(calculator_pid, { :add , value}) 
+  end
   defp loop(current_value) do
     receive do
       :value -> current_value
+      { :add , value } -> current_value =  current_value + value
     end
     loop(current_value)
   end
--- test/elixir_exercises_calculator_test.exs
+++ test/elixir_exercises_calculator_test.exs
@@ -1,12 +1,17 @@
 defmodule ElixirExercises.CalculatorTest do 
   use ExUnit.Case
-  import ElixirExercises.Calculator, only: [start: 0 , value: 1 ]
-  setup_all  do 
+  import ElixirExercises.Calculator, only: [start: 0 , value: 1 , add: 2 ]
+  setup  do 
       context = %{ calculator_pid: start}
     {:ok, context}
   end
   test "The initial value must be 0", %{calculator_pid: calculator_pid}  do
     IO.puts inspect calculator_pid
     value(calculator_pid) == 0
+  end
+  test "The add operation " , %{calculator_pid: calculator_pid} do
+    IO.puts inspect calculator_pid
+    add(calculator_pid, 10)
+    value(calculator_pid) == 10
   end
 end
