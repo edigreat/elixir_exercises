0cd02bffe5e636ec7d192c34156c1e4285af27a2
edigreat <heriberto.galdamez@gmail.com>
2015-12-29 13:31:55 -0600

    change. test name without prefix

--- test/calculator_test.exs
+++ test/calculator_test.exs
@@ -7,87 +7,60 @@ defmodule ElixirExercises.CalculatorTest do
   end
   test "The initial value must be 0", %{calculator_pid: calculator_pid}  do
     value(calculator_pid)
-    receive do
-      message ->   assert message == 0
-      after 1 -> assert false 
-    end
+    assert_receive 0 , 1
   end
 
   test "The add operation must be current_value plus the parameter " , %{calculator_pid: calculator_pid} do
     add(calculator_pid, 10)
     value(calculator_pid)
-    receive do
-      message ->  assert message  == 10
-      after 1 -> assert false 
-    end
+    assert_receive 10 , 1
   end
   
   
   test "The sub  operation must be current_value minus the parameter " , %{calculator_pid: calculator_pid} do
     sub(calculator_pid, 10)
     value(calculator_pid)
-    receive do
-      message ->  assert message  == -10
-      after 1 -> assert false 
-    end
+    assert_receive -10 , 1
   end
 
-  
-  
- 
-  test "The mul operation must be current_value multiplied by the paramter ", %{calculator_pid: calculator_pid} do
+  test "The mul operation must be current_value multiplied by the parameter ", %{calculator_pid: calculator_pid} do
+    add(calculator_pid,5)
     mul(calculator_pid, 10)
     value(calculator_pid) 
-    receive do
-      message ->  assert message  == 0 
-      after  1 -> assert false 
-    end
+    assert_receive 50 , 1
   end
 
   test "The div operation with current value equals zero must return 0 ", %{calculator_pid: calculator_pid}  do
     ElixirExercises.Calculator.div(calculator_pid,1)
     value(calculator_pid)
-    receive do
-      message ->  assert message  == 0 
-      after 1 -> assert false 
-    end
+    assert_receive 0 , 1
   end
 
   test "The div operation with parameter zero must return the same current value ", %{calculator_pid: calculator_pid}  do
     add(calculator_pid,10)
     ElixirExercises.Calculator.div(calculator_pid,0)
     value(calculator_pid) 
-    receive do
-      message ->  assert message  == 10 
-      after 1 -> assert false 
-    end
+    assert_receive 10 , 1
   end
   
   test "The div operation must be current value divide by parameter ", %{calculator_pid: calculator_pid}  do
     add(calculator_pid,10)
     ElixirExercises.Calculator.div(calculator_pid,5)
     value(calculator_pid) 
-    receive do
-      message -> assert message == 2.0  
-      after 1 -> assert false
-    end
+    assert_receive 2.0 , 1
   end
 
-  test "test exercise calculator", %{calculator_pid: calculator_pid} do
+  test "Exercise calculator", %{calculator_pid: calculator_pid} do
     value(calculator_pid)
+    assert_receive 0 , 1
+
     add(calculator_pid,10)
     sub(calculator_pid,5)
     mul(calculator_pid,3)
     ElixirExercises.Calculator.div(calculator_pid,5)
     value(calculator_pid)
-    receive do
-      message -> assert message == 0  
-      after 1 -> assert false
-    end
-    receive do
-      message -> assert message == 3.0  
-      after 1 -> assert false
-    end
+    assert_receive 3.0 , 1
+
   end
 
 end
